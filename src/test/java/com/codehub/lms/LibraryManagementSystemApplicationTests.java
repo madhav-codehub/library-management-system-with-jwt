16de8c550fa9502bcb04b78bd8e44a4366b7d311
madhav-codehub <madhav.p.akv@gmail.com>
2025-10-09 15:28:11 +0530

    initial commit

---
+++ src/test/java/com/codehub/lms/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehub.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
